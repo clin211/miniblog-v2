f76965e42e3ef5d41cb53c5ebbbfd43d9e4cb375
clin211 <767425412lin@gmail.com>
2025-03-31 23:13:22 +0800

    feat: 使用 viper 添加配置信息读取

---
+++ cmd/mb-apiserver/app/options/options.go
@@ -0,0 +1,73 @@
+package options
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/clin211/miniblog-v2/internal/apiserver"
+	"github.com/spf13/pflag"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// 定义支持的服务器模式集合
+var availableServerModes = sets.New(
+	"grpc",
+	"grpc-gateway",
+	"gin",
+)
+
+type ServerOptions struct {
+	// ServerMode 定义服务器模式 gRPC、Gin HTTP、HTTP Reverse Proxy
+	ServerMode string `json:"server-mode" mapstructure:"server-mode"`
+	// JWTKey 定义 JWT 秘钥
+	JWTKey string `json:"jwt-key" mapstructure:"jwt-key"`
+	// Expiration 定义 JWT Token 过期时间
+	Expiration time.Duration `json:"expiration" mapstructure:"expiration"`
+}
+
+// NewServerOptions 创建带有默认值的 ServerOptions 实例
+func NewServerOptions() *ServerOptions {
+	return &ServerOptions{
+		ServerMode: "grpc-gateway",
+		JWTKey:     "Rtg8BPKNEf2mB4mgvKONGPZZQSaJWNLijxR42qRgq0iBb5",
+		Expiration: time.Hour * 2,
+	}
+}
+
+func (o *ServerOptions) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&o.ServerMode, "server-mode", o.ServerMode, fmt.Sprintf("Server mode, available options: %v", availableServerModes.UnsortedList()))
+	fs.StringVar(&o.JWTKey, "jwt-key", o.JWTKey, "JWT signing key. Must be at least 6 characters long.")
+
+	// 绑定 JWT Token 的过期时间选项到命令行标志
+	// 参数名称 `--expiration`，默认值为 o.Expiration
+	fs.DurationVar(&o.Expiration, "expiration", o.Expiration, "JWT expiration")
+}
+
+// Validate 检验 ServerOptions 中的选项是否合法
+func (o *ServerOptions) Validate() error {
+	errs := []error{}
+
+	// 校验 ServerMode 是否有效
+	if !availableServerModes.Has(o.ServerMode) {
+		errs = append(errs, fmt.Errorf("invalid server mode: %s", availableServerModes.UnsortedList()))
+	}
+
+	// 校验 JWTKey 长度
+	if len(o.JWTKey) < 6 {
+		errs = append(errs, errors.New("JWTKey must be at least 6 characters long"))
+	}
+
+	// 合并所有错误并返回
+	return utilerrors.NewAggregate(errs)
+}
+
+// Config 基于 ServerOptions 创建新的 apiserver.Config。
+func (o *ServerOptions) Config() (*apiserver.Config, error) {
+	return &apiserver.Config{
+		ServerMode: o.ServerMode,
+		JWTKey:     o.JWTKey,
+		Expiration: o.Expiration,
+	}, nil
+}
